dabbfb3600eb5100cabebb0bf0d8d5b6bf6a55ab
chironcodes <adiltoncss@gmail.com>
2022-04-19 19:32:32 -0400

    finished airflow

--- dags/fetcher_sch.py
+++ dags/fetcher_sch.py
@@ -14,6 +14,9 @@ base_path_hdfs='/user/hadoop/airflow'
 year,month, day = date.today().strftime('%Y.%m.%d').split('.')
 
 
+def _failure(context):
+    print("On callback failures")
+    print(f"task { ti.task_id } failed in dag { ti.dag_id }")
 
 default_args = {
     'retries': 5,
@@ -38,43 +41,42 @@ hdfs_bash_exists = """hdfs dfs -test -d {{ params.path }};
 
 hdfs_bash_mkdir = """hdfs dfs -mkdir {{ params.path }}"""
 
-def _failure(context):
-    print("On callback failures")
-    print(f"task { ti.task_id } failed in dag { ti.dag_id }")
 
-with DAG(dag_id='staging_to_dlake', default_args=default_args,
+with DAG(dag_id='stage_to_dlake', default_args=default_args,
  schedule_interval='@monthly', start_date=days_ago(30), catchup=False) as dag:
 
   
 
     staging_area_sensor = FileSensor(
         task_id= 'staging_area_sensor',
-        fs_conn_id=base_path_hdfs,
+        fs_conn_id='my_staging_area',
         filepath='*',
-        poke_interval=10
+        poke_interval=5,
+        timeout=20
     )
 
-    #check if path exists R->True/False
-    hdfs_check_folder = BashOperator(
-        task_id='hdfs_check_folder',
-        bash_command=hdfs_bash_exists,
-        params={'path' : base_path_hdfs},
-        log_response=True,
-    )
+    
+    # hdfs_check_folder = BashOperator(
+    #     task_id='hdfs_check_folder',
+    #     bash_command=hdfs_bash_exists,
+    #     params={'path' : base_path_hdfs+f'/{month}'},
+    # )
 
-    #
+
     hdfs_mkdir_folder = BashOperator(
         task_id='hdfs_mkdir_folder',
         bash_command=hdfs_bash_mkdir,
         params={'path': base_path_hdfs+f'/{month}'}
     )
 
-    #move files from the staging to hdfs folder
+    
     move_dlake = BashOperator (
         task_id='move_dlake',
-        bash_command=(f'hdfs dfs -put {staging_folder}/* {base_path_hdfs}')
+        bash_command=("hdfs dfs -put -f {{params.staging_folder}}/* {{params.base_path_hdfs}}"),
+        params={'staging_folder':staging_folder,'base_path_hdfs':base_path_hdfs},
     )
 
 
+staging_area_sensor >> hdfs_mkdir_folder >> move_dlake
 
-staging_area_sensor >> hdfs_check_folder
+
